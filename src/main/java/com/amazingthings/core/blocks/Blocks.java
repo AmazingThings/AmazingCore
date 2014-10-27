3b21b319c5e82575898aabf82e785979c80254cf
mathmods <mathias.beverfjord@gmail.com>
2014-10-27 16:45:12 +0100

    Update #1.0.2.3
    Added worldGen, Works?

---
+++ src/main/java/com/amazingthings/core/blocks/Blocks.java
@@ -0,0 +1,21 @@
+package com.amazingthings.core.blocks;
+
+//REMOVE WHEN NEEDED
+@Deprecated
+public class Blocks {
+
+	
+	/**
+	 * NOTHING HERE TOO SEE
+	 * SEE NOTE AT AMAZINGCORE: 65
+	 */
+	
+	public static void registerBlocks(){
+		//Nothing to register yet
+	}
+	
+	public static void registerModelBlocks(){
+		//Nothing to register yet
+	}
+	
+}
